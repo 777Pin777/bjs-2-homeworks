1358fcb1d339e33d07921a1f51273dad8f911385
777Pin777 <100763326+777Pin777@users.noreply.github.com>
2022-08-27 11:54:15 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -41,10 +41,3 @@ else {
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
-}

367aec28af327e6e757fd614fef6ad56456ad08d
777Pin777 <100763326+777Pin777@users.noreply.github.com>
2022-08-28 14:01:43 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,10 @@
 function compareArrays(arr1, arr2) {
-  let result = false;
-  if (arr1.length == arr2.length){
-    result = arr1.every((element, index) => {return element === arr2[index]} );
-  }
-  return result; // boolean
+  if (arr1.length === arr2.length)
+    return arr1.every((element, index) => element === arr2[index]);
+  else return false;
 }
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter((element,int,arr) => {
-    if (element > 0 && element % 3 === 0) {return element}
-    }).map(item => item*10);
-  return resultArr; // array
+  let result = arr.filter(i => i > 0).filter(i => i % 3 === 0).map(i => i * 10)
+  return result;
 }

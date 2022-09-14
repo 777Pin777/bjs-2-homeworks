fc3416ca874c19f508862b70ec6d75a00c82abb5
777Pin777 <100763326+777Pin777@users.noreply.github.com>
2022-09-14 16:06:09 +0300

    Update task.js

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,42 +1,134 @@
 //                                  Задание №1
-
 class PrintEditionItem {
-    constructor(name, releaseDate, pagesCount) {
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
-        this.state = 100;
-        this.type = null;
-    }
+  constructor(...arg) { 
+ [this.name, this.releaseDate, this.pagesCount]= arg
+  this.state = 100;
+  this.type = null;
+  }
+ 
+  get state(){
+      return this._state
+  }
 
-    fix() {
-        this.state *= 1.5;
-    }
+  set state(value) {
+      if (value < 0) {
+        return this._state = 0
+      }else if(value > 100){ 
+        return this._state = 100
+      }else {
+        return this._state = value
+      }   
+  }  
+  fix() { 
+    this.state  = this.state * 1.5
+    return picknick.state = this._state 
+   }  
+}   
 
-    set actualState(num) {
-        if (num < 0) {
-            this.state = 0;
-        } if (num > 100) {
-            this.state = 100;
-        } else {
-            this.state = num;
-        }
-        return num;
-    }
-    get fullState() {
-        return this.state;
-    }
-}
-const sherlock = new PrintEditionItem(
-    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
-    2019,
-    1008
+let picknick = new PrintEditionItem(
+  "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+  2019,
+  1972 
 );
 
-console.log(sherlock.releaseDate); //2019
-console.log(sherlock.state); //100
-sherlock.fix();
-console.log(sherlock.state); //100
+  picknick.state = 10;
+  console.log(picknick.state); //10
+  picknick.fix();
+  console.log(picknick.state); //15
+  console.log(picknick.type)
+
+class Magazine extends PrintEditionItem {
+  constructor(...arg) {
+  super(...arg);
+  this.type = "magazine"
+}
+}
+
+let magazine = new Magazine(
+  "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+  2019,
+  1972
+)
+
+//   magazine.state = 10;
+//   console.log(magazine.state); //10
+//   magazine.fix();
+//   console.log(magazine.state); //15
+//   console.log(magazine.type)
+
+class Book extends PrintEditionItem {
+  constructor(author,...arg) {
+  super(...arg);
+  this.author = author;
+  this.type = "book";
+}
+}
+
+let book = new Book(
+  "Артур Конан Дойл",
+  "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+  2019,
+  1972
+)
+
+//   book.state = 31;
+//   console.log(book.state); //10
+//   book.fix();
+//   console.log(book.state); //15
+//   console.log(book.name);
+//   console.log(book.author);
+//   console.log(book.type);
+
+class NovelBook extends Book {
+  constructor(...arg) {
+  super(...arg);
+  this.type = "novel"
+}
+}
+
+let novelBook = new NovelBook(
+  "Герберт Уэллс",
+  "Машина времени",
+  1895,
+  138
+)
+
+class FantasticBook extends Book {
+  constructor(...arg) {
+  super(...arg);
+  this.type = "fantastic"
+  }
+}
+
+let fantasticBook = new FantasticBook(
+  "Аркадий и Борис Стругацкие",
+  "Пикник на обочине",
+  1972,
+  168
+)
+
+fantasticBook.state = 30;
+console.log(fantasticBook.state); //10
+fantasticBook.fix();
+console.log(fantasticBook.state); //15
+console.log(fantasticBook.name);
+console.log(fantasticBook.author);
+console.log(fantasticBook.type);
+console.log(fantasticBook);
+
+class DetectiveBook extends Book {
+  constructor(...arg) {
+  super(...arg);
+  this.type = "detective"
+}
+}
+
+let detectiveBook = new DetectiveBook(
+  "Артур Конан Дойл",
+  "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+  2019,
+  1008
+)
 
 //                                   Задание №2
 
